3935b7b47154ce3eb597f237a01ede60f80e0704
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-09-13 12:46:41 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,23 +7,29 @@ const routes = require("./routes");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://mongodb-service.ingress-nginx.svc.cluster.local:27017/todos", {
-    user: process.env.MONGO_INITDB_ROOT_USERNAME,
-    pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+  try {
+    await mongoose.connect(
+      `mongodb://${
+        process.env.MONGO_INITDB_ROOT_USERNAME
+      }:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongodb-service.ingress-nginx.svc.cluster.local:27017/${process.env.MONGO_INITDB_DATABASE}`, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
 
-  const app = express();
-  app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'] 
-  }));
-  app.use(express.json());
-  app.use("/api", routes);
+    const app = express();
+    app.use(cors({
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    }));
+    app.use(express.json());
+    app.use("/api", routes);
 
-  app.listen(port, () => {
-    console.log(`Server is listening on port: ${port}`);
-  });
+    app.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
+    });
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+  }
 }
